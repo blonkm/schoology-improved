65e0e06e2cbcd92f47e98332089ace599482baae
Michiel van der Blonk <blonkm@gmail.com>
2018-03-15 11:46:31 -0400

    automatic redirect
    closes #21

---
+++ index.php
@@ -0,0 +1,16 @@
+<?
+function redirect($url, $permanent = false)
+{
+    if (headers_sent() === false) {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+    else {
+      echo "Headers already sent in $filename on line $linenum\n" .
+          "Cannot redirect, for now please click this <a " .
+          "href=\"/schoology/groups.php?action=courses\">link</a> instead\n";
+  }
+    exit();
+}
+
+redirect('/schoology/groups.php?action=courses', false);
+?>
