780d238d9dd0ee958be9fa3c58b43b4ae3166594
Michiel van der Blonk <blonkm@gmail.com>
2020-01-26 06:44:15 -0400

    closes #48 absolute path in groups.php
    changed to just 'groups.php' instead of the full link. If necessary we will add a 'root' folder to configuration.

--- groups.php
+++ groups.php
@@ -213,7 +213,7 @@ require_once('controller.php');
             <?}?>
             <?foreach ($groups as $group=>$groupId) { ?>
                     <tr>
-                        <td><a href="http://labs.bluekoala-aruba.com/schoology/groups.php?section=<?=$section?>&action=groups&group=<?=$group?>"><?=$group?></a></td><?
+                        <td><a href="groups.php?section=<?=$section?>&action=groups&group=<?=$group?>"><?=$group?></a></td><?
                     foreach ($assignments as $id=>$assignment) { ?>
                         <td><a target="_blank" title="<?=$group?> - <?=$assignment?>" href="groups.php?section=<?=$section?>&group=<?=urlencode($group)?>&assignment=<?=$id?>&action=files">&nbsp;</a></td>
                     <?}?>
